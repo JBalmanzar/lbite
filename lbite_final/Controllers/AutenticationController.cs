5e65864c2592a9448f02e40237a78f7ee3691c6d
Bryan Astacio <brya0196@gmail.com>
2018-12-02 19:45:44 -0400

    agregado el long

--- lbite_final/Controllers/AutenticationController.cs
+++ lbite_final/Controllers/AutenticationController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace lbite_final.Controllers
@@ -7,8 +9,31 @@ namespace lbite_final.Controllers
         // GET
         public ActionResult Index()
         {
-            return
-            View();
+            return View();
+            
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            string User = Request.Form["user"];
+            string Password = Request.Form["password"];
+            
+            if (User.Length >= 5 && Password.Length >= 5)
+            {
+                return View("Index");
+            }
+            else
+            {
+                if (User.Length < 5) ViewBag.ErrorUsuario = "El ususario debe tener minimo 5 caracteres";   
+                
+                if (Password.Length < 5) ViewBag.ErrorPassword = "El password debe tener minimo 5 caracteres";
+                
+
+                return View("Index");
+            }
+
+            
         }
     }
 }
